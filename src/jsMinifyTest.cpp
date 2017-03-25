1cde497d056248d5c34bfea3813940e91a83d3e3
D8bauxit8 <jehodamark.net@gmail.com>
2017-03-25 11:54:23 +0100

    Created JS test class and header

---
+++ src/jsMinifyTest.cpp
@@ -0,0 +1,12 @@
+#include "jsMinifyTest.h"
+
+void jsMinifyTest::setSource(std::string str) {
+
+}
+void jsMinifyTest::test(void) {
+
+}
+
+std::string jsMinifyTest::getSource(void) {
+
+}
---
+++ src/jsMinifyTest.h
@@ -0,0 +1,21 @@
+#ifndef JSMINIFYTEST_H
+#define JSMINIFYTEST_H
+
+#include <string>
+#include "jsminifyproc.h"
+#include "sourcecode.h"
+
+class jsMinifyTest
+{
+public:
+	void setSource(std::string str);
+	void test(void);
+	std::string getSource(void);
+
+private:
+	std::string str;
+	jsMinifyProc jsMin;
+	sourceCode source;
+};
+
+#endif // JSMINIFYTEST_H
